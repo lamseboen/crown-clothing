acc5b41407998e11ee8ace823067032ad01dae9f
lamse <lamse@lamses-MacBook-Pro.local>
2020-01-13 23:40:50 +0700

    adding sign in form

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Route, Switch, Link } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInAndSignupPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route  path='/shop' component={ShopPage} />
+          <Route  path='/login' component={SignInAndSignupPage} />
         </Switch>
     </div>
   );
